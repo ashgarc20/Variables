bcfc7f31b7c605731d9464a9fd10828263e60977
Ashley <ashleyagarcia@student.hartnell.edu>
2017-09-24 23:50:27 +0000

    Different Variable Types

---
+++ Variable.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    //<endl; gives you a new line 
+    // char is an indivisual element 
+    // and I could refernce characters in the ASCII table
+    cout << char(65) <<endl;
+    
+    // an indivisual number 
+    cout << int(65) <<endl;
+    /*
+    cout << short int   // small whole number
+    cout << long int   // long whole number
+    */
+    
+    // false is qual to zero
+    // true is equal to one
+    cout << false <<endl;
+    
+    // Floating point number 
+    cout << float(33.5) <<endl;
+    
+    // Double percision floating point number
+    cout << double(33) <<endl;
+    // cout << long double
+    // 8cout << wchar_t
+    
+    return 0;
+}
